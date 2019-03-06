4cb785356c1ac61566df4fb035e839e6821f2bed
Mike Choi <mike.choi@live.ca>
2019-03-05 23:12:44 -0800

    Added Palindrome Number question - Edit

--- Palindrome_Number/padlindromeNumber.js
+++ Palindrome_Number/padlindromeNumber.js
@@ -21,22 +21,16 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-  if(x < 0) {
+  if(x < 0 || (x % 10 === 0 && x != 0)) {
     return false;
   }
   
   let reverse = 0;
-  let xCopy = x;
   
-  while(xCopy !== 0) {
-    let mod = xCopy % 10;
-    xCopy = Math.floor(xCopy / 10);
-    reverse = reverse * 10 + mod;
+  while(x > reverse) {
+    reverse = reverse * 10 +  x % 10;
+    x = Math.floor(x / 10);
   }
   
-  if(reverse === x) {
-    return true;
-  } 
-  
-  return false;
+  return reverse === x || Math.floor(reverse/10) === x;
 };
