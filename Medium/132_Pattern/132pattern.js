6d25b135ed137ed1a5eaa2a4cddeef4bdc971eb3
Mike Choi <mike.choi@live.ca>
2019-03-16 14:48:01 -0700

    Added 132 Pattern

---
+++ Medium/132_Pattern/132pattern.js
@@ -0,0 +1,69 @@
+// Given a sequence of n integers a1, a2, ..., an, a 132 pattern is a subsequence ai, aj, ak such that i < j < k and ai < ak < aj. Design an algorithm that takes a list of n numbers as input and checks whether there is a 132 pattern in the list.
+
+// Note: n will be less than 15,000.
+
+// Example 1:
+// Input: [1, 2, 3, 4]
+// Output: False
+// Explanation: There is no 132 pattern in the sequence.
+
+// Example 2:
+// Input: [3, 1, 4, 2]
+// Output: True
+// Explanation: There is a 132 pattern in the sequence: [1, 4, 2].
+
+// Example 3:
+// Input: [-1, 3, 2, 0]
+// Output: True
+// Explanation: There are three 132 patterns in the sequence: [-1, 3, 2], [-1, 3, 0] and [-1, 2, 0].
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var find132pattern = function(nums) {
+  if(nums.length < 3) return false;
+  
+  let stack = [];  
+  let last = -Infinity;
+  for(let j = nums.length-1; j >= 0; j--) {
+    if(last > nums[j]) return true;
+    
+    while(stack[stack.length-1] < nums[j]) last = stack.pop();
+    
+    stack.push(nums[j])
+  }
+  
+  return false;
+};
+
+// OR 
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var find132pattern = function(nums) {
+  if(nums.length < 3) {
+    return false;
+  }
+  
+  let intervals = [];
+  let s = 0;
+  let i = 1;
+  
+  while(i < nums.length) {
+    if(nums[i] <= nums[i-1]) {
+      if(s < i - 1) intervals.push([nums[s], nums[i-1]]);
+      s = i;      
+    }
+    
+    for(let j = 0; j < intervals.length; j++) {
+      if(nums[i] > intervals[j][0] && nums[i] < intervals[j][1]) return true;
+    }
+    
+    i++;
+  }
+  
+  return false;
+};
