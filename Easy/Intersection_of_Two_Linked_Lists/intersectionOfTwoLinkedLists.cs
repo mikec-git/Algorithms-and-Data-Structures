05e6a0dbdb14c1992f598de3e01114c83bf49631
Mike Choi <mike.choi@live.ca>
2019-03-20 01:44:36 -0700

    Added Intersection of Two Linked Lists

--- Easy/Intersection_of_Two_Linked_Lists/intersectionOfTwoLinkedLists.cs
+++ Easy/Intersection_of_Two_Linked_Lists/intersectionOfTwoLinkedLists.cs
@@ -68,3 +68,21 @@ public class Solution {
     return start;
   }
 }
+
+// OR 
+
+public class Solution {
+  public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+    if(headA == null || headB == null) return null;
+    
+    var pA = headA;
+    var pB = headB;
+    
+    while(pA != pB) {
+      pA = pA == null ? headB : pA.next;
+      pB = pB == null ? headA : pB.next;
+    }
+    
+    return pA;
+  }
+}
