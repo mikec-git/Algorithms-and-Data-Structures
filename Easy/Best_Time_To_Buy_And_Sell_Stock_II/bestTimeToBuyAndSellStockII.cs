b50e4c6293c773d33c92c7a1382872a77be8c5f4
Mike Choi <mike.choi@live.ca>
2019-04-01 19:15:25 -0700

    Added Best Time to Buy and Sell Stock II

--- Easy/Best_Time_To_Buy_And_Sell_Stock_II/bestTimeToBuyAndSellStockII.cs
+++ Easy/Best_Time_To_Buy_And_Sell_Stock_II/bestTimeToBuyAndSellStockII.cs
@@ -51,3 +51,19 @@ public class Solution {
     return sum;
   }
 }
+
+// OR 
+
+// public class Solution {
+//   public int MaxProfit(int[] prices) {
+//     if(prices.Length < 2) return 0;
+    
+//     var sum = 0;
+    
+//     for(var i = 1; i < prices.Length; i++) {
+//       if(prices[i] > prices[i-1]) sum += prices[i] - prices[i-1];
+//     }
+    
+//     return sum;
+//   }
+// }
