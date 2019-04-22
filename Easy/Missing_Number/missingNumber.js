e718759410f6335342f103226c9e6dd7f6b4c4e1
Mike Choi <mike.choi@live.ca>
2019-04-22 14:08:33 -0700

    Added Missing Number

--- Easy/Missing_Number/missingNumber.js
+++ Easy/Missing_Number/missingNumber.js
@@ -25,3 +25,19 @@ var missingNumber = function(nums) {
   
   return missing;
 };
+
+// OR 
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+  let missing = nums.length;
+  
+  for(let i = 0; i < nums.length; i++) {
+    missing ^= i ^ nums[i];
+  }
+  
+  return missing;
+};
